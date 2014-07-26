dac046203ce47cc67f3403c592fda11f63d636ef
DuphusDigitalProductions <99nolan@gmail.com>
2014-07-26 16:28:00 -0700

    Commit 1: The Full Source For Version 1.0.0.0
    The full source code for version 1.0.0.0

---
+++ BatterIDE/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ICSharpCode.TextEditor.Document;
+using System.IO;
+
+namespace BatterIDE
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textEditorControl1_Load(object sender, EventArgs e)
+        {
+            string dirc = Application.StartupPath;
+            FileSyntaxModeProvider fsmp;
+            if (Directory.Exists(dirc))
+            {
+                fsmp = new FileSyntaxModeProvider(dirc);
+                HighlightingManager.Manager.AddSyntaxModeFileProvider(fsmp);
+                textEditorControl1.SetHighlighting("BAT");
+            }
+        }
+    }
+}
